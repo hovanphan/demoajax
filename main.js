9fb5cdca8e8b3a0d9c45bf3ac1eb5cc8962159b0
hovanphan <50516326+hovanphan@users.noreply.github.com>
2020-01-12 10:08:57 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 function ajaxFrom() {
-    $(button).click(function (e) {
+    $('button').click(function (e) {
         e.preventDefault();
         const url = $(this).attr('data-url');
         console.log(url);
-        const email = $('email').val();
+        const email = $('#email').val();
         console.log(email);
-        const passWord = $('password').val();
+        const passWord = $('#password').val();
         console.log(passWord);
-        const checkboxfrom = $("checkboxfrom").val();
+        const checkboxfrom = $("#checkboxfrom").val();
         console.log(checkboxfrom);
-    })
-    $.ajax({
-        type: 'POST',
-        url: url,
-        data: {
-            email: email,
-            passWord: passWord,
-            checkboxfrom: checkboxfrom
-        },
-        dataType: 'dataType',
 
-    })
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                email: email,
+                passWord: passWord,
+                checkboxfrom: checkboxfrom
+            },
 
-}
+        });
+    });
+
+};
 $(document).ready(function () {
     ajaxFrom();
 });
